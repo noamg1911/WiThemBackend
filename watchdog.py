8167b8f23725a0ad569e96bf9e5787fd410c95cd
Noam Glassman <noamg1911@gmail.com>
2022-09-04 18:58:14 +0300

    watchdog can send sms

--- watchdog.py
+++ watchdog.py
@@ -9,25 +9,25 @@ mov = MovementAlert()
 alerters = [mov]
 
 
-# def get_anomaly(alerter):
-#     anomaly_state, anomaly_description = alerter.check_anomaly()
-#     return anomaly_description if anomaly_state else None
+def user_query_to_contacts(user, contacts):
+    query_result = elastic_query_handler.query_data("search-users", {'terms': {'id': user}})
+    user_contacts = query_result[0]["_source"]["contacts"]
+    for contact in user_contacts:
+        for phone_number in contact.values():
+            contacts.add(phone_number)
 
 
-def send_sms(content, contacts):
-    sms_sender.send_sms(content, contacts)
-    alerter.sms_sent = True
-
-
-def anomaly_manager(alerter):
-    anomaly = alerter.check_anomaly()
-    if anomaly:
-        for anomaly_message in anomaly:
-            contacts = set()
-            for user in anomaly[anomaly_message]:
-                elastic_query_handler.query_data("search-users", "{'query': {'terms': {'_id': [{}] }}}".format(user))
+def sms_manager(anomaly):
+    for anomaly_message in anomaly:
+        contacts = set()
+        for user in anomaly[anomaly_message]:
+            user_query_to_contacts(user, contacts)
+            sms_sender.send_sms(anomaly_message, contacts)
 
 
 def check_alerters():
     for alerter in alerters:
-        anomaly_manager(alerter)
+        anomaly = alerter.check_anomaly()
+        if anomaly:
+            sms_manager(anomaly)
+            alerter.sms_sent = True
