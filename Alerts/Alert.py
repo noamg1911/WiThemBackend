4aeb69d74de42eacdef91566c2860e750ffabbf6
Tal Fuks <talfuks1234@gmail.com>
2022-09-04 18:21:39 +0300

    Finished Alert Structure and Movement

--- Alerts/Alert.py
+++ Alerts/Alert.py
@@ -1,13 +1,22 @@
 from ElasticHandler.ElasticHandler import ElasticHandler
 
-class Alert():
+
+class Alert:
+    """
+    Master class for Alerts, used to check anomalys with elastic querys and sends the summarized results.
+    """
+    SMS_STRUCTURE = "{specific_alert}"
     def __init__(self):
         self.es = ElasticHandler()
         self.sms_sent = False
         self.state = False
 
     def check_anomaly(self):
-        raise("Function not used!")
+        data = self._query_sensor()
+        return self._summarized_results(data)
 
-    def _update_alert(self):
-        raise("Function not used!")
+    def _query_sensor(self):
+        raise "Function not used!"
+
+    def _summarized_results(self, data_summary):
+        raise "Function not used!"
--- Alerts/MovementAlert.py
+++ Alerts/MovementAlert.py
@@ -1,11 +1,44 @@
-from Alert import Alert
+import datetime
+from Alerts.Alert import Alert
+
+ITERATION_TIME = 30 * 60
+ALERT_MESSAGE = "Phone hasn't been moved in a long time."
+
 
 class MovementAlert(Alert):
+    """
+    Used to send specific gyro related alerts.
+    """
     def __init__(self):
         super().__init__()
 
-    def check_anomaly(self):
-        pass
+    def _query_sensor(self):
+        """
+        Querys events from the last iteration which are related to gyroscope sensors
+        """
+        current_time = int(datetime.datetime.timestamp(datetime.datetime.now()))
+        data = self.es.query_data("search-event", {"bool": { "must": [{
+                                                                  "match": {
+                                                                    "Sensor": "Gyroscope"
+                                                                  }},
+                                                                {
+                                                                  "range": {
+                                                                    "Timestamp": {
+                                                                        "gte": current_time - ITERATION_TIME
+                                                                        }
+                                                                  }
+                                                                }]}})
+        return data if data is not {} else None
 
-    def _update_alert(self):
-        pass
+    def _summarized_results(self, data_summary):
+        """
+        Summarizes the results and sends them with the correct SMS message.
+        :param data_summary:
+        :return:
+        """
+        if data_summary is not None:
+            agent_ids = []
+            for event in data_summary:
+                agent_ids.append(event['_source']['AgentID'])
+            return {self.SMS_STRUCTURE.format(specific_alert=ALERT_MESSAGE): list(set(agent_ids))}
+        return None
